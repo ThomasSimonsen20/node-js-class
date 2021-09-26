9afc0010b89f9cc06c0efee863f2336173993fe0
Thomas Simonsen <thom622g@stud.kea.dk>
2021-09-26 09:40:27 +0200

    started on dankmemes crud

--- 1._Servers/1._Dankmemes/app.js
+++ 1._Servers/1._Dankmemes/app.js
@@ -1,29 +1,76 @@
-const express = require("express");
-const app = express();
+const app = require("express")()
+const express = require("express")
+app.use(express.json())
 
-const memes = [{
-    1 : "Drake Hotline Bling",
-    2 : "Two buttons",
-    3 : "Distracted Boyfriend",
-    4 : "Change my mind",
-    5 : "Gru's Plan",
-    6 : "Sad Pablo Escobar",
-    7 : "Anakin Padme 4 panel",
-    8 : "Epic Handshake",
-    9 : "Disaster Girl",
-    10 : "Surprised Pikachu"
-}];
+let memes = [
+    {id: 1, name : "Drake Hotline Bling"},
+    {id: 2, name : "Two buttons"},
+    {id: 3, name : "Distracted Boyfriend"},
+    {id: 4, name : "Change my mind"},
+    {id: 5, name : "Gru's Plan"},
+    {id: 6, name : "Sad Pablo Escobar"},
+    {id: 7, name : "Anakin Padme 4 panel"},
+    {id: 8, name : "Epic Handshake"},
+    {id: 9, name : "Disaster Girl"},
+    {id: 10, name : "Surprised Pikachu"}
+]
 
 app.get("/dankmemes", (req, res) => {
-    res.send({memes: memes});
-});
+    res.send({memes})
+})
 
 
+app.get("/dankmemes/:id", (req, res) => {
+    res.send(memes[req.params.id-1])
+})
+
+app.patch("/dankmemes/:id", (req, res) => {
+    memes.forEach(meme => {
+        if (meme.id == req.params.id) {
+            meme.name = req.body.name
+            res.sendStatus(201) 
+        }
+    })
+    res.sendStatus(404)
+}) 
+
+/*
+app.patch("/dankmemes/:id", (req, res) => {
+    memes.forEach((meme, index) => {
+        if (meme.id == req.params.id) {
+            meme.name = req.body.name
+            console.log(meme.name)
+            memes[index].name = meme.name
+            
+        }
+    })
+    res.send({memes})
+}) 
+
+
+app.patch("/dankmemes/:id", (req, res) => {
+    memes.forEach((meme) => {
+        if(meme.id === req.params.id) {
+            memes[index] = req.body
+            res.send({memes})
+        }
+    })
+})
+*/
+
+app.delete("/dankmemes/:id", (req, res) => {
+    const index = memes.indexOf(req.params.id)
+    if (index > -1) {
+        memes.splice(index, 1)
+    }
+    res.send({memes})
+    console.log(memes)
+})
 
 
 app.listen(8080, (error) => {
     if (error) {
-        console.log(error);
+        console.log(error)
     }
     console.log("The server is running on port", 8080);
-});
+})
