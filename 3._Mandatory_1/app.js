c6081a611436f24c396e0a62f48e3e533ae2d75f
Thomas Simonsen <thom622g@stud.kea.dk>
2021-10-06 10:59:59 +0200

    contiuned work on mandatory assignment

--- 3._Mandatory_1/app.js
+++ 3._Mandatory_1/app.js
@@ -1,24 +1,29 @@
 const express = require("express")
 const app = express()
 
-app.use(express.static('images'))
+app.use(express.json())
+app.use(express.static('public'))
 app.use(express.static('files'))
 
 
 app.get("/", (req,res) => {
-    res.sendFile(__dirname + "/public/html/index.html")
+    res.sendFile(__dirname + "/public/index/index.html")
 })
 
 app.get("/express", (req,res) => {
-    res.sendFile(__dirname + "/public/html/express.html")
+    res.sendFile(__dirname + "/public/express/express.html")
 })
 
 app.get("/tools", (req,res) => {
-    res.sendFile(__dirname + "/public/html/tools.html")
+    res.sendFile(__dirname + "/public/tools/tools.html")
 })
 
 app.get("/fundamentals", (req,res) => {
-    res.sendFile(__dirname + "/public/html/fundamentals.html")
+    res.sendFile(__dirname + "/public/fundamentals/fundamentals.html")
+})
+
+app.get("/leftoverNotes", (req,res) => {
+    res.sendFile(__dirname + "/public/leftover_notes/leftover_notes.html")
 })
 
 app.listen(8080, (error) => {
@@ -27,3 +32,4 @@ app.listen(8080, (error) => {
     }
     console.log("Server is running on port", 8080)
 })
+
