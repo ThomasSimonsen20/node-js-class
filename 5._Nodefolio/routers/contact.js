6d16f91bbbb709e97eea4414069642d5e7f62110
Thomas Simonsen <thom622g@stud.kea.dk>
2021-11-06 12:09:19 +0100

    done frontpage, header and footer styling

--- 5._Nodefolio/routers/contact.js
+++ 5._Nodefolio/routers/contact.js
@@ -17,24 +17,20 @@ router.post("/api/contact", (req,res) => {
     let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 587,
-        secure: false, // true for 465, false for other ports
+        secure: false, 
         auth: {
-          user: "nodemailertest202@gmail.com", // generated ethereal user
-          pass: "nodemailer20", // generated ethereal password
+          user: "nodemailertest202@gmail.com", 
+          pass: "nodemailer20", 
         },
     });
     
-      // send mail with defined transport object
       transporter.sendMail({
-        from: '"Nodemailer contact" <nodemailertest202@gmail.com>', // sender address
-        to: "bestpalaeu20@gmail.com", // list of receivers
-        subject: "Node contact request", // Subject line
-        text: "Hello world?", // plain text body
-        html: output, // html body
+        from: '"Nodemailer contact" <nodemailertest202@gmail.com>', 
+        to: "bestpalaeu20@gmail.com", 
+        subject: "Node contact request", 
+        text: "Hello world?", 
+        html: output, 
     });
-
-
-
     
     res.redirect("/contact")
 })
