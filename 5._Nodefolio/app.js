9e4f9c216a86f2477be8ae78be8b68c1f5e750f8
Thomas Simonsen <thom622g@stud.kea.dk>
2021-11-13 10:18:14 +0100

    changed endpoints to router/pages

--- 5._Nodefolio/app.js
+++ 5._Nodefolio/app.js
@@ -6,12 +6,11 @@ import express from "express"
 const app = express()
 
 app.use(express.static('public'))
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
 //const { createPage } = require("./render.js")
-import createPage from "./render.js"
+//import createPage from "./render.js"
 
 /*
 const projectsRouter = require("./routers/projects.js")
@@ -23,54 +22,9 @@ import projectsRouter from "./routers/projects.js"
 import pageRouter from "./routers/pages.js"
 import contactRouter from "./routers/contact.js"
 
-/*
-app.use(projectsRouter.router)
-app.use(pageRouter.router)
-app.use(contactRouter.router)
-*/
 app.use(projectsRouter)
 app.use(pageRouter)
 app.use(contactRouter)
-
-/* Ready the pages */
-const frontpagePage = createPage("frontpage/frontpage.html", { title: "Nodefolio | Welcome"})
-const projectsPage = createPage("projects/projects.html")
-const createProjectPage = createPage("dashboard/createProject.html")
-const updateProjectPage = createPage("dashboard/updateProject.html")
-const cvPage = createPage("cv/cv.html")
-const contactPage = createPage("contact/contact.html")
-const dashboardPage = createPage("dashboard/dashboard.html")
-
-
-
-app.get("/", (req, res) => {
-    res.send(frontpagePage)
-})
-
-app.get("/projects", (req, res) => {
-    res.send(projectsPage)
-})
-
-app.get("/cv", (req, res) => {
-    res.send(cvPage)
-})
-
-app.get("/contact", (req, res) => {
-    res.send(contactPage)
-})
-
-app.get("/dashboard", (req, res) => {
-    res.send(dashboardPage)
-})
-
-app.get("/createProject", (req, res) => {
-    res.send(createProjectPage)
-})
-
-app.get("/updateProject", (req, res) => {
-    res.send(updateProjectPage)
-})
-
 
 const PORT = process.env.PORT || 8080;
 
--- 5._Nodefolio/routers/pages.js
+++ 5._Nodefolio/routers/pages.js
@@ -1,15 +1,43 @@
-//const router = require("express").Router();
-
 import express from "express"
 const router = express.Router()
 
-router.get("/ballade", (req,res) => {
-    res.send("<h1>Sjov</h1>")
+import createPage from "../render.js"
+
+const frontpagePage = createPage("frontpage/frontpage.html", { title: "Nodefolio | Welcome"})
+const projectsPage = createPage("projects/projects.html")
+const createProjectPage = createPage("dashboard/createProject.html")
+const updateProjectPage = createPage("dashboard/updateProject.html")
+const cvPage = createPage("cv/cv.html")
+const contactPage = createPage("contact/contact.html")
+const dashboardPage = createPage("dashboard/dashboard.html")
+
+
+router.get("/", (req, res) => {
+    res.send(frontpagePage)
+})
+
+router.get("/projects", (req, res) => {
+    res.send(projectsPage)
+})
+
+router.get("/cv", (req, res) => {
+    res.send(cvPage)
+})
+
+router.get("/contact", (req, res) => {
+    res.send(contactPage)
+})
+
+router.get("/dashboard", (req, res) => {
+    res.send(dashboardPage)
+})
+
+router.get("/createProject", (req, res) => {
+    res.send(createProjectPage)
+})
+
+router.get("/updateProject", (req, res) => {
+    res.send(updateProjectPage)
 })
 
 export default router
-
-/*
-module.exports = {
-    router
-} */
