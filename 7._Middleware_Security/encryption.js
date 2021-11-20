d1f453fd63eeb4537ddb9156f3eb102593128dc9
Thomas Simonsen <thom622g@stud.kea.dk>
2021-11-20 09:46:28 +0100

    test

--- 7._Middleware_Security/encryption.js
+++ 7._Middleware_Security/encryption.js
@@ -1,15 +1,16 @@
 import bcrypt from "bcrypt";
 
 const saltRounds = 12;
-const correctPassword = "hunter123";
+const correctPassword = "admin";
 const incorrectPassword = "Hunter123";
-const correctHash = "";
+const correctHash = "$2b$12$beyAXdjja3fypP9dredMBOlE38F4.30J4v2RhWzq696jv47urxFxm";
 
 (async () => {
     const hashedPassword = await bcrypt.hash(correctPassword, saltRounds);
-    console.log(hashedPassword);
+    //console.log(hashedPassword);
 
     const isPasswordCorrect = await bcrypt.compare(correctPassword, correctHash)
+    console.log(isPasswordCorrect)
 })()
 
 export default {}
